f0078fe54b65952201f8ec6ba876a8a707123bdc
Rahul Gopal Patil <rahulil@cybage.com>
2022-11-21 20:51:55 +0530

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Home from './component/Home';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./component/Home";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store,persistor } from "./component/redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import AddEmployee from "./component/AddEmployee";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="addemployee" element={<AddEmployee />} />
+        </Routes>
+      </Router>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
